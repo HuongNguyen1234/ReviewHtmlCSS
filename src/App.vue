e4a50fce6cda124b4d3fc2e84e88946837effc1e
huongnguye1234 <huongnguyenthihuong1234@gmail.com>
2019-08-14 23:09:18 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <router-view />
+    <div id="grid">
+      <div id="item1">11111111</div>
+      <div id="item2">2</div>
+      <div id="item3">3</div>
+      <div id="item4">4</div>
+      <div id="item5">5</div>
+      <div id="item6">6</div>
+      <div id="item6">7</div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+<style lang="scss" scoped>
+.app {
+  #grid {
+    // height: 200px;
+    width: 100%;
+    margin: 20px;
+    display: grid;
+    // grid: repeat(2, 60px) / auto-flow 30px;
+    // grid: auto-flow 100px / repeat(4, 1fr);
+    justify-content: center;
+    // grid-gap: 10px;
+    justify-items: start;
+    grid-template-columns: 500px 500px 150px;
+    grid-template-rows: 1fr minmax(80px, 100px);
+    // grid-auto-flow: row dense; /* or 'row', 'row dense', 'column dense' */
+    #item1 {
+      background-color: lime;
+      // grid-column: span 2;
+    }
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    #item2 {
+      background-color: yellow;
+      // grid-row: span 2;
+    }
+
+    #item3 {
+      background-color: blue;
+      // grid-column: span 1;
+    }
+
+    #item4 {
+      // grid-column-start: 2;
+      background-color: red;
+    }
+
+    #item5 {
+      background-color: aqua;
+    }
+  }
 }
 </style>
---
+++ src/demoImage.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="https://picsum.photos/id/237/300/300" />
+    </div>
+    <div class="vertical">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="big">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="vertical">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="horizontal">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="big">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="horizontal">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="big">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div>
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+    <div class="vertical">
+      <img src="https://picsum.photos/id/237/100/300" />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  margin: 50px;
+  grid-gap: 5px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  // grid-template-rows: repeat(auto-fill, minmax(100px, 1fr));
+  // grid-template: repeat(5, 50px) / repeat(2, 1fr);
+  grid-auto-rows: 300px;
+  grid-auto-flow: row dense;
+  div {
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .horizontal {
+    // grid-column: span 4 / 3;
+    grid-column: span 2;
+  }
+  .vertical {
+    grid-row: span 2;
+  }
+
+  .big {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+}
+</style>
